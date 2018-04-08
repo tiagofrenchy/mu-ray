8e3ed0136b193cd946a61370c53b2c08ae8ada5f
tiagofrenchy <37619246+tiagofrenchy@users.noreply.github.com>
2018-04-08 10:36:23 -0300

    Update analise.R

--- mu-ray_R/t_test_plus_ANOVA_analysis/analise.R
+++ mu-ray_R/t_test_plus_ANOVA_analysis/analise.R
@@ -17,11 +17,13 @@ results <- read_csv("Resultado dos testes estatísticos.csv")
 
 p_value = .05
 
+
 #### Selecionando genes que respeitam os pressupostos do ANOVA
+
 listUniqueTranscripts <- unique(results$transcript_cluster_id)
 numberTranscripts <- length(listUniqueTranscripts)
 
-results[,"segue todos os pressupostos ANOVA"] <- NA
+results[,"segue todos os pressupostos ANOVA"]  <- NA
 results[,"segue todos os pressupostos T-test"] <- NA
 
 for (i in seq(numberTranscripts)) {
@@ -50,8 +52,8 @@ results <- results[c("transcript_cluster_id", "description", "normD1", "normD2",
                      "p_ctrlDia1", "p_ctrlDia2", "p_ctrlDia3", "p_ctrlDia4", "p_ctrlSepsis")]
 
 
-
 #### Genes estudando
+
 dipep <- results %>% filter(transcript_cluster_id == 8002181)
 tnfr <- results %>% filter(transcript_cluster_id == 8149733)
 
@@ -66,6 +68,7 @@ dadosGene <- clean_data %>% filter(transcript_cluster_id == gene)
 
 
 ##### FUNÇÃO GRÁFICA 02
+
 boxplot(expression ~ Dia, data = dadosGene)
 
 # Cria boxplot das espressões
@@ -79,16 +82,19 @@ boxplot(expression ~ Dia, data = dadosGene)
 
 ###### FUNÇÃO GRÁFICA 01, IMPORTADA DO ARQUIVO ANTIG
 # Obtemos nome e descrição do gene.
+
 nomeT <- clean_data %>% filter(transcript_cluster_id == gene) %>% select(description)
 nome <- sprintf("%s", nomeT[1][1,])
 
 
 # Agora, listamos as comparações que serão realizadas.
+
 my_comparisons <- list( c("D1", "D2"), c("D2", "D3"),
                         c("D3", "D4"), c("D1", "D3"), c("D1", "D4"),
                         c("D2", "D4"))
 
 # Por fim, fazemos o gráfico.
+
 setwd("~/IC Alexandre/Imgs/")
 
 png(sprintf("%s.png",nome))
@@ -100,8 +106,10 @@ groupExpression$Dia <- ordered(groupExpression$Dia, levels = c("D1", "D2", "D3",
 controlExpression <- clean_data %>% filter(transcript_cluster_id == gene, Dia == 'D0') %>% 
   select(expression)
 
+
 # Calculamos a altura máxima do boxplot para que o gráfico fique com proporções
 # decentes.
+
 altura = max(groupExpression$expression) + 0.1
 
 img <- ggboxplot(groupExpression, main = nome, x = "Dia", y = "expression",
@@ -116,10 +124,11 @@ plot(img)
 dev.off()
 
 
-#
+##### Code Dumpster #####
+
 # Nos links abaixo, parece que descobriram como esconder a comparação
 # quando não é significante. Mas tá dando trabalho pra caralho para entender
-# # o que fizeram
+# o que fizeram
 # https://stackoverflow.com/questions/45476950/r-ggplot2-boxplots-ggpubr-stat-compare-means-not-working-properly
 # https://stackoverflow.com/questions/45552715/r-ggplot2-perform-pairwise-tests-per-pair-in-a-facet-and-show-the-p-values-wit
 # https://stackoverflow.com/questions/46446392/r-ggplot2-boxplots-with-significance-level-more-than-2-groups-kruskal-test-an
@@ -128,50 +137,47 @@ dev.off()
 # DEBUG
 
 
-}
-
-#
 # stat_compare_means(comparisons = my_comparisons)+ # Add pairwise comparisons p-value
 #   stat_compare_means(label.y = 50)     # Add global p-value
-#
-#
-#
+
+
 # ggboxplot(ToothGrowth, x = "dose", y = "len",
 #           color = "dose", palette = "jco")+
 #   stat_compare_means(comparisons = my_comparisons, label.y = c(29, 35, 40))+
 #   stat_compare_means(label.y = 45)
-#
-#
+
+
 # ggboxplot(groupExpression, x = "Dia", y = "expression",
 #           color = "Dia", palette = c("#00AFBB", "#E7B800", "#FC4E07", "#000000"))+
 #   stat_compare_means(comparisons = my_comparisons, label.y = c(29, 35, 40))+
 #   stat_compare_means(label.y = 45)
+
+# ggboxplot(dado, x = "Dia", y = "expression",
+#           color = "Dia", palette = c("#00AFBB", "#E7B800", "#FC4E07", "#000000"),
+#           order = c("D1", "D2", "D3", "D4"),
+#           ylab = "Expressão", xlab = "Dia")
+
 #
-# # ggboxplot(dado, x = "Dia", y = "expression",
-# #           color = "Dia", palette = c("#00AFBB", "#E7B800", "#FC4E07", "#000000"),
-# #           order = c("D1", "D2", "D3", "D4"),
-# #           ylab = "Expressão", xlab = "Dia")
-#
-# #
-# # Se não der certo, rode um Kruskall Wallis
-# # kruskal.test(weight ~ group, data = my_data)
-#
-#
-#
+# Se não der certo, rode um Kruskall Wallis
+# kruskal.test(weight ~ group, data = my_data)
+
+
+
 # ################
 # #h<-hist(groupExpression$expression, breaks = 7, col="white")
 # ################
+
+
+#   ###############
+#   media_log <- mean(dado$log_expression)
+
+
+
+# DEBUG
+#   print(sprintf('%8.3f  %5.1f%%', media_log, i/numberTranscripts*100), quote = FALSE)
 #
+#   log_averages [nrow(log_averages) + 1,] = c(listUniqueTranscripts[i], media_log)
+# }
 #
-# #
-# #   ###############
-# #   media_log <- mean(dado$log_expression)
-# #
-# #   # DEBUG
-# #   print(sprintf('%8.3f  %5.1f%%', media_log, i/numberTranscripts*100), quote = FALSE)
-# #
-# #   log_averages [nrow(log_averages) + 1,] = c(listUniqueTranscripts[i], media_log)
-# # }
-# #
-# # return (log_averages)
-# # }
+# return (log_averages)
+# }
